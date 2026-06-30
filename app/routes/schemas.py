bd864279aeae465542dc2bb55a8095b0d245ffc9
Thaneluna <42688917+Thaneluna@users.noreply.github.com>
2026-06-29 20:42:51 -1000

    Added additional field classes to for issues schema

--- app/routes/schemas.py
+++ app/routes/schemas.py
@@ -11,3 +11,23 @@ class issue_priority(str, Enum):
     low="low"
     medium="medium"
     high="high"
+
+# Creates a new issue with required fields
+class create_issues(BaseModel):
+    title: str = Field(min_length=2, max_length=100, example="Issue Title")
+    description: Optional[str] = Field(None, max_length=100, example="Issue Description")
+    priority: issue_priority = issue_priority.medium
+
+# Want every field to be optional for update, so that user can update any field they want
+class update_issues(BaseModel):
+    title: Optional[str] = Field(None, min_length=2, max_length=100, example="Issue Title")
+    description: Optional[str] = Field(None, max_length=1000, example="Issue Description")
+    priority: Optional[issue_priority] = None
+    status: Optional[issue_status] = None
+
+class issue_out(BaseModel):
+    id: str
+    title: str
+    description: str
+    priority: issue_priority
+    status: issue_status
